978602e66ecbf877342dcad67bfd8613f8b08dcf
jiaheng.song <jiaheng.song@whaty.com>
2021-12-14 11:23:20 +0800

    实现数据迁移逻辑；添加数据可信度属性

--- liteExampleVer2/data.js
+++ liteExampleVer2/data.js
@@ -1,27 +1,63 @@
 window.LIKE_DATA = {
     __DATA_RUNTIME: [],
-    staticData: {
+    DATA_ROOT: {
         clickTimes: {
             __RT_META: {
+                __LINK_TO: "/newTimeInfo/newTimes",
+                __CONFIDENCE_LEVEL: 0,
                 __WATCHERS: {},
                 __OWNER: null
             },
             __DATA: 0
+        },
+
+        newTimeInfo: {
+            __RT_META: {
+                __CONFIDENCE_LEVEL: 0,
+                __WATCHERS: {},
+                __OWNER: null
+            },
+            __DATA: {
+                newTimes: {
+                    __RT_META: {
+                        __CONFIDENCE_LEVEL: 0,
+                        __WATCHERS: {},
+                        __OWNER: null
+                    },
+                    __DATA: 0
+                }
+            }
         }
     }
 }
 
 window.LIKE_DATA_UTILS = {
     getDataByPath: function (db, dataPath) {
-        var staticData = db.staticData;
+        var dataRoot = db.DATA_ROOT;
         var paths = dataPath.split("/");
-        var currentObj = staticData;
+        var currentObj = dataRoot;
         for (var pathIndex in paths) {
+            //如果当前路径对象为非根对象，则需要取出数据段
+            if(currentObj !== dataRoot) {
+                currentObj = currentObj.__DATA;
+            }
+            //获取一个路径
             var path = paths[pathIndex];
+            //路径为空路径，跳过
+            if(path === "") continue;
+            //如果当前路径对象为空，直接返回
             if(!currentObj) return null;
             currentObj = currentObj[path];
         }
-        return currentObj;
+        var objLinkBefore = currentObj;
+        //数据被迁移：目前只支持根数据迁移
+        if(currentObj.__RT_META.__LINK_TO)
+        {
+            return this.getDataByPath(db, currentObj.__RT_META.__LINK_TO);
+        }
+        else {
+            return currentObj;
+        }
     },
 
     bindingViewLogic: function (view, logic) {
